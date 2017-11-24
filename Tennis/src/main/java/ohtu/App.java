a3520f385002563244deacad8f5746daca64d2ad
wileht <wille.lehtomaki@helsinki.fi>
2017-11-24 19:40:06 +0200

    tennistä

---
+++ Tennis/src/main/java/ohtu/App.java
@@ -0,0 +1,24 @@
+package ohtu;
+
+public class App {
+	public static void main(String[] args) {
+	    TennisGame game = new TennisGame("player1", "player2");
+
+	    System.out.println(game.getScore());
+
+	    game.wonPoint("player1");
+	    System.out.println(game.getScore());
+
+	    game.wonPoint("player1");
+	    System.out.println(game.getScore());
+
+	    game.wonPoint("player2");
+	    System.out.println(game.getScore());
+
+	    game.wonPoint("player1");
+	    System.out.println(game.getScore());
+
+	    game.wonPoint("player1");
+	    System.out.println(game.getScore());
+	}
+}
